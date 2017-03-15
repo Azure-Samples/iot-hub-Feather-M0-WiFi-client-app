0099a80b005e1ce2aadb8d79096afae65cd4a603
Yuwei Zhou <yuwzho@microsoft.com>
2017-03-15 17:57:32 +0800

    Add client application (#1)
    * init
    
    * add readme
    
    * travis
    
    * travis
    
    * update
    
    * update
    
    * RTCZero
    
    * update
    
    * update
    
    * update
    
    * memory is not freed

---
+++ app/config.h
@@ -0,0 +1,26 @@
+// Physical device information for board and sensor
+#define DEVICE_ID "Feather M0 WiFi"
+
+// Pin layout configuration
+#define LED_PIN 13
+#define BME_CS 5
+
+// Interval time(ms) for sending message to IoT Hub
+#define INTERVAL 2000
+
+// If don't have a physical DHT sensor, can send simulated data to IoT hub
+#define SIMULATED_DATA false
+
+
+// SSID and SSID password's length should < 32 bytes
+// http://serverfault.com/a/45509
+#define SSID_LEN 32
+#define PASS_LEN 32
+#define CONNECTION_STRING_LEN 256
+
+#define MESSAGE_MAX_LEN 256
+
+#define WINC_EN 2
+#define WINC_CS 8
+#define WINC_IRQ 7
+#define WINC_RST 4
